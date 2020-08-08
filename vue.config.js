ea0bdfede06000ce6ee823fc56414dbfdf190623
Arnaldo Souza <arnaldoeloi2@gmail.com>
2020-08-07 23:17:20 -0300

    Application title applied to pages on vue.config.js.

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,13 @@ module.exports = {
     themeColor: '#4aaeff',
     name: 'Vue Weather Dashboard',
   },
-  publicPath: '/vue-weather-conditions'
+  publicPath: '/vue-weather-conditions',
+  pages:{
+    index: {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Weather Conditions',
+    }
+  }
 }
